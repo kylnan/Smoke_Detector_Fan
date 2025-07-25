25e999dd762babfad51c52000f39d629542ac836
kylnan <kyl3zyl3@gmail.com>
2025-07-24 22:18:04 -0700

    testing out fade example for RTOS/GPIO comprehension and usage

--- main/main.c
+++ main/main.c
@@ -1,25 +1,61 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_err.h"
 
-#define LED_GPIO GPIO_NUM_5
-
-void setup(void) {
-    // This function can be used to set up the GPIO or other peripherals if needed
-    // Currently, it does nothing
-}
+gpio_num_t LED_GPIO = GPIO_NUM_5;
 
 void blink_task(void *pvParameter) {
     gpio_reset_pin(LED_GPIO);
     gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
-
+    
+    /* blink loop
     while(1) {
         gpio_set_level(LED_GPIO, 1); // Turn LED on
         vTaskDelay(pdMS_TO_TICKS(500)); // Wait for 500 ms
         gpio_set_level(LED_GPIO, 0); // Turn LED off
-        vTaskDelay(pdMS_TO_TICKS(500)); // Wait for 500 ms
+        vTaskDelay(pdMS_TO_TICKS(30)); // Wait for 500 ms
     }
+
+    */
+    // fade loop
+    ledc_timer_config_t timer = {
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .duty_resolution = LEDC_TIMER_12_BIT,
+            .timer_num = LEDC_TIMER_0,
+            .freq_hz = 1000, // Frequency of the PWM signal
+            .clk_cfg = LEDC_AUTO_CLK
+        };
+    ledc_channel_config_t channel = { 
+            .speed_mode = LEDC_LOW_SPEED_MODE,
+            .channel = LEDC_CHANNEL_0,
+            .timer_sel = LEDC_TIMER_0,
+            .intr_type = LEDC_INTR_DISABLE,
+            .gpio_num = LED_GPIO,
+            .duty = 0, // Initial duty cycle
+            .hpoint = 0
+        };
+
+    ledc_timer_config(&timer);
+    ledc_channel_config(&channel);
+    
+    int duty = 0;
+    int increment = 5;
+    while(1){
+        ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty);
+        ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+        duty += increment;
+        if (duty >= 255 || duty <= 0) {
+            increment = -increment; // Reverse the direction of fading
+        }
+        vTaskDelay(pdMS_TO_TICKS(30)); // Wait for the specified delay time
+    }
+
 }
 
 void normal_mode(void *pvParameter) {
@@ -32,5 +68,5 @@ void exhaust_mode(void *pvParameter) {
     // Currently, it does nothing
 }
 void app_main() {
-    xTaskCreate(blink_task, "blink_task", 2048, NULL, 5, NULL);
+    xTaskCreate(blink_task, "blink_task", 4096, NULL, 1, NULL);
 }
